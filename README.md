bdab7b3118553fcddaa1c1adb18b267be468e12b
Marc <marcusher@outlook.com>
2022-05-16 09:22:20 +0100

    Updated README and js file with initial notes/process

---
+++ README.md
@@ -0,0 +1,20 @@
+## Marc Usher - Project 01
+
+## Introduction
+Some words about this project.
+
+## Technologies Used
+
+## Idea Development
+* [Trello Board with Wireframes & User Stories](https://trello.com/b/5KDX4WUr/ga-sei-64-project-1)
+* Link to wireframes and user stories.
+* Document your planning and tell a story about your development process and problem-solving strategy.
+
+## Unfinished Business
+* List unsolved problems which would be fixed in future iterations.
+
+## Winning Condition
+* Describe how you solved for the winner
+
+## Fully Functioning
+* Describe how some of your favorite functions work
--- project-folder/js/main.js
+++ project-folder/js/main.js
@@ -1,1 +1,23 @@
-// NOTES
+// Initial notes
+
+// Hover state to show possible moves for a piece
+// Click & drop or drag & drop for moves; 
+// Click to select piece to push/be pushed - click again to deselect?
+// Rules for whether or not a piece can be pushed (anchor = NO, blocked by rails at the top/bottom = NO, if falling off the board (have invisible squares there?) then there's a little animation of the piece falling off
+
+
+// First steps/notes
+
+// Empty grid
+// Objects on top
+// Container is min width
+// Object move logic is done by %
+// Draw a line to follow the mouse & snap to grid
+//   - Click once to draw a line, click again to confirm teh move/press a button to cancel the move/
+// Local storage = storage your move or the game state? Or you click reset game that's the only way? Also save scores?
+
+
+// First phase
+
+// Get start pages & grid set up 
+// Create the Pieces on top that you can move one step at time
