f779c665b96546036e014067bde1a82eab54f538
Marc <marcusher@outlook.com>
2022-05-19 16:21:26 +0100

    Updated all project files - HTML removed tokens as now added in JS; CSS updated styling for Forfeit pop-up; js functionality update and code tidying

--- js/main.js
+++ js/main.js
@@ -29,16 +29,6 @@ let setupDroppables = false;
 
 
 
-// RULES BUTTON POP-UP
-
-$('.button.rules').click(function() {
-    $('.pop-up').toggle();
-})
-
-$('.pop-up').click(function() {
-    $('.pop-up').toggle();
-})
-
 
 // Audio effects
 
@@ -59,6 +49,21 @@ function playSoundPickup() {
     }
     playSoundbite(`audio/pickup.wav`);
 }
+
+
+
+
+// POP-UPS
+
+// RULES POP-UP
+$('.button.rules').click(function() {
+    $('.rules.pop-up').toggle();
+})
+
+$('.rules.pop-up').click(function() {
+    $('.rules.pop-up').toggle();
+})
+
 
 
 // TOKEN DROPPED SUCCESSFULLY
@@ -132,28 +137,6 @@ function enableDroppables($elements) {
 
 
 
-// START GAME
-// Button click to start the game and move us into the first game phase of 'setup'.
-
-
-$('.game-prompt p').html("Welcome to <b>Push Fight</b>, a game of skill and surprise! Two players battle on a game board as the white team plays the green team.  The goal is for a player to push ONE of their opponent's game pieces off the board at either end. <b>Click the button to start the game</b>.");
-$('.button.skip p').html("<b>Start Game</b>");
-
-$('.button.skip').click(function() {
-        setup();   
-    })
-
-// when the game starts destroy all the logic from init()
-// https://api.jqueryui.com/droppable/#method-destroy
-
-
-// DRAGGABLE & DROPPABLE SETUP
-// Allows both players to position their pieces on the game board, with rules governing which can be picked up/placed governed by the individual turn functions and the updateGame() function.
-// The function calls the updateGame() function, which is where the main game logic functionality is stored.
-
-
-
-
 
 // ON TOKEN PICKUP, this function runs to check the game board for which grid items have tokens already, and stops players from placing their tokens there. The main game logic happens here on the PICKUP of a token.
 
@@ -214,6 +197,21 @@ function updateGame($token) {
                 }
             }
          }
+
+
+        // GAME LOGIC FOR MOVE TURNS
+        // if (game.status === "whitemove") {
+        //     // PICK UP SQUARE
+        //     // CHECK ALL ELEMENTS ON ROW
+        //     // WE'RE IN OUR GRID LOOP HERE, 
+        //     // $(value) = current grid item in the loop;
+        //     // $token is the token that's picked up/being dragged
+        //     // all tokens have data position = to their grid square
+        //     // As part of this loop, we need to retrieve the data positions of all the occupied squares eg. 4-3 (dragged token), 1-4 2-4 3-4 4-4 (other whites), 1-5 2-5 3-5 4-5 1-6 (greens)
+        //     // ALREADY can't drop on somewhere with an existing element
+        //     // if $(value) has class ui-droppable-hover && grid--occupied then can it IMMEDIATELY revert? can you interrupt a drag (drag: functionality for draggable?)
+        //     // or set it so it can't move within a certain distance of an object?
+        // }
     
         if (game.status === "whitepush" || game.status === "greenpush") {
             if (gridOccupied) { // && adjacent
@@ -233,9 +231,7 @@ function updateGame($token) {
                 $theGridSquare.addClass('grid--unoccupied');
                 enableDroppables($theGridSquare);
             }
-            
-
-            // wrap game status in not(greenpush, whitepush)
+    
             // loop through grid to find all grid occupied? or
             // variables for picked up token data-row & data-column;
             // if data-row+-1 or data-column +-1 has.Class(grid--occupied) THEN ONLY ALLOW THOSE AS DROPPABLE IN THIS MOVE SET
@@ -244,19 +240,6 @@ function updateGame($token) {
             // then need to add to the drop function if dropping onto occupied space then fire a new function (pass in to that function what direction we care about ie. row/column, + or -) and then for the rest of the ones in that row it shimmies them along
         }
         
-
-        // if (game.status === "whitemove") {
-        //     // PICK UP SQUARE
-        //     // CHECK ALL ELEMENTS ON ROW
-        //     // WE'RE IN OUR GRID LOOP HERE, 
-        //     // $(value) = current grid item in the loop;
-        //     // $token is the token that's picked up/being dragged
-        //     // all tokens have data position = to their grid square
-        //     // As part of this loop, we need to retrieve the data positions of all the occupied squares eg. 4-3 (dragged token), 1-4 2-4 3-4 4-4 (other whites), 1-5 2-5 3-5 4-5 1-6 (greens)
-        //     // ALREADY can't drop on somewhere with an existing element
-        //     // if $(value) has class ui-droppable-hover && grid--occupied then can it IMMEDIATELY revert? can you interrupt a drag (drag: functionality for draggable?)
-        //     // or set it so it can't move within a certain distance of an object?
-        // }
     });
 
 
@@ -295,10 +278,38 @@ function updateGame($token) {
 
 
 
+// START GAME
+// Initial function to set up teh tokens, update the prompt at the top of the screen and allow you to click the Start Game button.
+
+function init() {
+
+    game.status = "start";
+
+    $('#green-score').append('<div class="token token--green token--square"></div><div class="token token--green token--square"></div><div class="token token--green token--square"></div><div class="token token--green token--circle"></div><div class="token token--green token--circle"></div>')
+
+    $('#white-score').append('<div class="token token--white token--square"></div><div class="token token--white token--square"></div><div class="token token--white token--square"></div><div class="token token--white token--circle"></div><div class="token token--white token--circle"></div>')
+
+    $('.game-prompt p').html("Welcome to <b>Push Fight</b>, a game of skill and surprise! Two players battle on a game board as the white team plays the green team.  The goal is for a player to push ONE of their opponent's game tokens off the board at either end. <b>Click the button to start the game</b>.");
+
+    $('.button.skip p').html("<b>Start Game</b>");
+
+    $('.button.skip').click(function() {
+        setup();   
+    })
+}
+
+init()
+
+
+
+
 // GAME TURN FUNCTIONS
 
 // GAME SETUP
 // Button click clears the element taking the draggable property and updates it before calling the function for the next turn, whiteMove().
+
+// This function also allows both players to position their pieces on the game board, with rules governing which can be picked up/placed governed by the individual turn functions and the updateGame() function.
+// Calls the updateGame() function each time a token is picked up, which is where the main game logic functionality is stored.
 
 function setup() {
     game.status = "setup"; 
@@ -353,13 +364,12 @@ function setup() {
     }
 
 
-// FIRST PHASE - WHITE TURN
+// FIRST PHASE - WHITE MOVE
 // Updates the game prompt, while the rest of the functionality stays the same as in the setup() function.
 // Button click clears the element taking the draggable property before calling the function for the next turn, whitePush().
 
 function whiteMove () {
     game.status = "whitemove";
-   
     $('.game-prompt p').html("<b>White, move your tokens!</b>. You can move up to two of your tokens to an empty space on the board, as long as you can get there by moving unimpeded in horizontal and vertical moves. Click the button when you've completed the moves you wanted to make.");
 
     $('.button.skip p').html("<b>Finish white move</b>");
@@ -370,18 +380,127 @@ function whiteMove () {
         whitePush();
         
     })
+
+    forfeit();
 }
 
-
+// SECOND PHASE - WHITE PUSH
+// Updates the game prompt, while the rest of the functionality stays the same as in the setup() function.
+// Button click clears the element taking the draggable property before calling the function for the next turn, greenMove().
 
 
 function whitePush () {
     game.status = "whitepush";
-    $('.game-prompt p').html("<b>White, push with one of your square tokens!</b>");
+    $('.game-prompt p').html("<b>White, use one of your square tokens to push ONE space!</b>");
 
     $('.button.skip p').html("<b>Finish white push</b>");
     $('.button.skip').off("click");
-    // $('.button.skip').click(function() {
-        // game.status = "whitepush";  
-        //  whitePush();
+    $('.button.skip').click(function() {
+        setupDraggables.draggable( "option", "cancel", "" )
+        setupDraggables.draggable( "option", "cancel", ".token--white" )
+        greenMove();
+    })
+
+    forfeit();
 }
+
+
+// THIRD PHASE - GREEN MOVE
+// Updates the game prompt, while the rest of the functionality stays the same as in the setup() function.
+// Button click clears the element taking the draggable property before calling the function for the next turn, greenPush().
+
+function greenMove () {
+    game.status = "greenmove";
+   
+    $('.game-prompt p').html("<b>Green, move your tokens!</b>. You can move up to two of your tokens to an empty space on the board, as long as you can get there by moving unimpeded in horizontal and vertical moves. Click the button when you've completed the moves you wanted to make.");
+
+    $('.button.skip p').html("<b>Finish green move</b>");
+    $('.button.skip').off("click");
+    $('.button.skip').click(function() {
+        setupDraggables.draggable( "option", "cancel", "" );
+        setupDraggables.draggable( "option", "cancel", ".token--circle, .token--white");
+        greenPush();
+    })
+
+    forfeit();
+}
+
+
+// FOURTH PHASE - GREEN PUSH
+// Updates the game prompt, while the rest of the functionality stays the same as in the setup() function.
+// Button click clears the element taking the draggable property before calling the function for the next turn, going back to whiteMove(). This loops until somebody wins or forfeits the game.
+
+
+function greenPush () {
+    game.status = "greenpush";
+    $('.game-prompt p').html("<b>Green, use one of your square tokens to push ONE space!</b>");
+
+    $('.button.skip p').html("<b>Finish green push</b>");
+    $('.button.skip').off("click");
+    $('.button.skip').click(function() {
+        setupDraggables.draggable( "option", "cancel", "" );
+        setupDraggables.draggable( "option", "cancel", ".token--green" );
+        whiteMove();
+    })
+
+    forfeit();
+}
+
+
+
+// FORFEIT POP-UP
+// Logic to show the forfeit pop-up during Push turns for both players, in case they're unable to push and need to forfeit the game.
+
+function forfeit() {
+    if (game.status === "whitepush") {
+        $('.button.forfeit').show();
+        $('.button.forfeit').click(function() {
+            game.status = "end";
+            $('.button.rules').after("<div class='forfeit pop-up'><p>Looks like <b>White</b> has forfeited as they can't push any tokens! Better luck next time!<br><br>Refresh the page to reset the game.</p></div>");
+           
+            $('forfeit.pop-up').toggle();
+    
+            // $('.forfeit.pop-up').click(function() {
+            //     reset();
+            // })
+        })
+    } else if (game.status === "greenpush") {
+        $('.button.forfeit').show();
+        $('.button.forfeit').click(function() {
+            game.status = "end";
+            $('.button.rules').after("<div class='forfeit pop-up'><p>Looks like <b>Green</b> has forfeited as they can't push any tokens! Better luck next time!<br><br>Refresh the page to reset the game.</p></div>");
+           
+            $('forfeit.pop-up').toggle();
+    
+            // $('.forfeit.pop-up').click(function() {
+                // reset();
+            // })
+        })
+    } else {
+        $('.button.forfeit').toggle();
+    }
+}
+
+forfeit();
+
+
+
+// GAME WIN FUNCTION
+// Function to be called if certain win conditions are met. Currently not working as Push logic still needs to be finalised.
+
+
+// function gameWin () {
+
+// }
+
+
+
+// RESET FUNCTION
+// To reset the game after a forfeit or game win. Currently not working.
+
+
+// function reset () {
+//     $('.forfeit.pop-up').hide();
+//     $('.token').remove();
+//     init();
+// }
